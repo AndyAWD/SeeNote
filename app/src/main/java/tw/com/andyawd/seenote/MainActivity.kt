de06dc7c931df5a444eb96e4d260949f8ffcc6df
AndyAWD <anandydy529@gmail.com>
2020-08-07 22:20:16 +0800

    chore: 0.0.1
    hello world

---
+++ app/src/main/java/tw/com/andyawd/seenote/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.com.andyawd.seenote
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
