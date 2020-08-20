e2b0061a17446953464f40c6bd64375e6c1c92b9
AndyAWD <anandydy529@gmail.com>
2020-08-20 16:02:43 +0800

    feat: 建立筆記清單頁面

---
+++ app/src/main/java/tw/com/andyawd/seenote/noteinventory/NoteInventoryFragment.kt
@@ -0,0 +1,24 @@
+package tw.com.andyawd.seenote.noteinventory
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import tw.com.andyawd.seenote.R
+
+class NoteInventoryFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_note_inventory, container, false)
+    }
+
+
+}
