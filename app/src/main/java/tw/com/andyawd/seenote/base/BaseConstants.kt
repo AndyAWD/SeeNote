fec7efef073e509870b7e7147a19acbc9fca293f
AndyAWD <anandydy529@gmail.com>
2020-08-20 15:54:41 +0800

    feat: 建立BaseConstants

---
+++ app/src/main/java/tw/com/andyawd/seenote/base/BaseConstants.kt
@@ -0,0 +1,14 @@
+package tw.com.andyawd.seenote.base
+
+class BaseConstants {
+    companion object {
+        const val SEE_NOTE = "seeNote"
+        const val ID = "id"
+        const val TITLE = "title"
+        const val CONTENT = "content"
+        const val TIME = "time"
+        const val EMPTY_STRING = "emptyString"
+
+        const val ROOM_NOTE = "roomNote"
+    }
+}
